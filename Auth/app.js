d399f28c95e62fbb3e7850c0a989547376de0e13
Reshul Wate <reshulwate@gmail.com>
2025-12-25 13:45:08 +0530

    Cookies

---
+++ Auth/app.js
@@ -0,0 +1,22 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+
+
+const app = express();
+
+app.use(cookieParser())
+
+
+app.get("/", (req, res)=> {
+    res.cookie("name", "Reshul");
+    res.send("Success")
+});
+
+
+app.get("/read", (req, res)=>{
+    console.log(req.cookies)
+    res.send("Read Page!!");
+});
+
+
+app.listen(3000);
