33b38ce246e567d5e32626707678924772a3adc8
Reshul Wate <reshulwate@gmail.com>
2025-12-27 18:06:29 +0530

    Create user in mongodb

--- Auth/app.js
+++ Auth/app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const userModel = require("./models/user");
 
+const bcrypt = require("bcrypt");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -14,5 +15,26 @@ app.use(cookieParser());
 app.get("/", (req, res)=>{
     res.render("index")
 })
+
+app.post("/create", (req, res)=>{
+    let {username, password, email, age} = req.body;
+    let createUser;
+
+    bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(password, salt, async(err, hash)=>{
+
+            createUser = await userModel({
+                username,
+                password:hash,
+                email,
+                age
+            });
+
+            await createUser.save();
+            res.send(createUser)
+        })
+    })
+})
+
 
 app.listen(3000);
