884e97ec5a285c81abcb70c78a77cc85d5755390
Reshul Wate <reshulwate@gmail.com>
2025-12-27 10:44:53 +0530

    Bcrypt for passwords

--- Auth/app.js
+++ Auth/app.js
@@ -1,8 +1,10 @@
-// Getting started
-
+// Getting started - 3 items
+// Cookies
+// Password encryption and decryption 
+// JWT
 const cookieParser = require("cookie-parser");
 const express = require("express");
-
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -10,8 +12,17 @@ app.use(cookieParser())
 
 
 app.get("/", (req, res)=> {
-    res.cookie("name", "Reshul");
-    res.send("Success")
+    // making hash for password
+    bcrypt.genSalt(10, function (err, salt){
+        bcrypt.hash('password', salt, function (err, hash){
+            console.log(hash)
+        })
+    })
+
+    // check password match
+    bcrypt.compare('password', "$2b$10$3U6TJvO7dkuMZTUWpJA33OEt56mM8oGr3X5rYoHhJdahYWLOukWYq", function(err, res){
+        console.log(res)
+    })
 });
 
 
